b6de295f046f744dd2fe6d57a0ed94588e0c311d
Hiroaki Nakamura <hnakamur@gmail.com>
2009-07-04 03:02:09 +0900

    initial commit

---
+++ src/main/java/net/naruho/mycheckstyleexperiments/App.java
@@ -0,0 +1,13 @@
+package net.naruho.mycheckstyleexperiments;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
